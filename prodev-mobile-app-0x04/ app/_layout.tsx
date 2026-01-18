df03265bdda803c9ee861b7c77e803a8531ba305
tirachrach950-sys <tirachrach950@gmail.com>
2026-01-18 13:22:17 +0100

    Update _layout.tsx

--- prodev-mobile-app-0x04/ app/_layout.tsx
+++ prodev-mobile-app-0x04/ app/_layout.tsx
@@ -1,1 +1,16 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signin" />
+    </Stack>
+  );
+}
 
